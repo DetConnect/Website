71fb40069dfa1fb48618e51941e14ece796025c0
Brendan Kapp <brendaniskapp@gmail.com>
2022-03-05 12:26:12 -0500

    Added preliminary padding center join form

--- code/lib/widgets/join_message.dart
+++ code/lib/widgets/join_message.dart
@@ -27,199 +27,193 @@ class _JoinMessageState extends State<JoinMessage> {
   String _errorMessage = "";
 
   _JoinMessageState() {
-    _myAnimatedWidget = Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
+    _myAnimatedWidget = Row(
       children: [
-        TextCard(
-            title: "Join Us!",
-            text: "Fill out your details below and hit submit!"),
         Padding(
-          padding: const EdgeInsets.all(15),
+          padding: EdgeInsets.all(50),
         ),
-        TextFormField(
-          decoration: const InputDecoration(
-            icon: Icon(
-              Icons.person,
-              color: Colors.white,
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextCard(
+                title: "Join Us!",
+                text: "Fill out your details below and hit submit!"),
+            Padding(
+              padding: const EdgeInsets.all(15),
             ),
-            labelText: 'Name',
-            hintText: 'John Smith',
-            fillColor: Colors.white,
-            filled: true,
-          ),
-          style: TextStyle(
-            color: Colors.blue[900],
-            decorationColor: Colors.white,
-          ),
-          onChanged: (String value) {
-            this.name = value;
-          },
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10),
-        ),
-        TextFormField(
-          decoration: const InputDecoration(
-            icon: Icon(
-              FontAwesomeIcons.medal,
-              color: Colors.white,
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+                labelText: 'Full Name',
+                hintText: 'John Smith',
+                fillColor: Colors.white,
+                filled: true,
+              ),
+              style: TextStyle(
+                color: Colors.blue[900],
+                decorationColor: Colors.white,
+              ),
+              onChanged: (String value) {
+                this.name = value;
+              },
             ),
-            labelText: 'Rank',
-            hintText: 'C/Lt Col',
-            fillColor: Colors.white,
-            filled: true,
-          ),
-          style: TextStyle(
-            color: Colors.blue[900],
-            decorationColor: Colors.white,
-          ),
-          onChanged: (String value) {
-            this.rank = value;
-          },
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10),
-        ),
-        TextFormField(
-          decoration: const InputDecoration(
-            icon: Icon(
-              FontAwesomeIcons.sitemap,
-              color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.all(10),
             ),
-            labelText: 'Position',
-            hintText: 'Joint Liason Officer',
-            fillColor: Colors.white,
-            filled: true,
-          ),
-          style: TextStyle(
-            color: Colors.blue[900],
-            decorationColor: Colors.white,
-          ),
-          onChanged: (String value) {
-            this.position = value;
-          },
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10),
-        ),
-        TextFormField(
-          decoration: const InputDecoration(
-            icon: Icon(
-              FontAwesomeIcons.building,
-              color: Colors.white,
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(
+                  FontAwesomeIcons.medal,
+                  color: Colors.white,
+                ),
+                labelText: 'Rank',
+                hintText: 'C/Lt Col',
+                fillColor: Colors.white,
+                filled: true,
+              ),
+              style: TextStyle(
+                color: Colors.blue[900],
+                decorationColor: Colors.white,
+              ),
+              onChanged: (String value) {
+                this.rank = value;
+              },
             ),
-            labelText: 'Detachment',
-            hintText: 'Det 538',
-            fillColor: Colors.white,
-            filled: true,
-          ),
-          style: TextStyle(
-            color: Colors.blue[900],
-            decorationColor: Colors.white,
-          ),
-          onChanged: (String value) {
-            this.detachment = value;
-          },
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10),
-        ),
-        TextFormField(
-          decoration: const InputDecoration(
-            icon: Icon(
-              Icons.email,
-              color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.all(10),
             ),
-            labelText: 'Email',
-            hintText: 'yourname@college.edu',
-            fillColor: Colors.white,
-            filled: true,
-          ),
-          style: TextStyle(
-            color: Colors.blue[900],
-            decorationColor: Colors.white,
-          ),
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          onChanged: (String value) {
-            this.email = value;
-          },
-          validator: (String value) {
-            return (value != null && value.contains("@")) && value.contains(".")
-                ? null
-                : 'Invalid email address';
-          },
-        ),
-        // AnimatedOpacity(
-        //     child: RichText(
-        //       text: TextSpan(
-        //           style: DefaultTextStyle.of(context).style,
-        //           children: <TextSpan>[
-        //             TextSpan(
-        //               text: this._errorMessage,
-        //               style: TextStyle(
-        //                   fontWeight: FontWeight.bold,
-        //                   color: Colors.red,
-        //                   fontFamily: 'Roboto',
-        //                   fontSize: 30),
-        //             ),
-        //           ]),
-        //     ),
-        //     opacity: this._errorOpacity,
-        //     duration: const Duration(milliseconds: 100)),
-        MouseRegion(
-          cursor: SystemMouseCursors.click,
-          child: GestureDetector(
-            child: const Icon(
-              Icons.play_arrow,
-              size: 100,
-              color: Colors.white,
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(
+                  FontAwesomeIcons.sitemap,
+                  color: Colors.white,
+                ),
+                labelText: 'Position',
+                hintText: 'Joint Liason Officer',
+                fillColor: Colors.white,
+                filled: true,
+              ),
+              style: TextStyle(
+                color: Colors.blue[900],
+                decorationColor: Colors.white,
+              ),
+              onChanged: (String value) {
+                this.position = value;
+              },
             ),
-            onTap: () {
-              if (this.name == null ||
-                  this.rank == null ||
-                  this.position == null ||
-                  this.detachment == null ||
-                  this.email == null) {
-                // for not filled out
-                print("not filled out");
-                setState(() {
-                  this._errorOpacity = 1;
-                  this._errorMessage = "Please fill out all fields!";
-                });
-              } else {
-                var response = sendRequest();
-                response.then((value) {
-                  if (value.statusCode == 200) {
-                    // response is good
-                    setState(() {
-                      this._errorOpacity = 0;
-                      this._myAnimatedWidget = Padding(
-                          padding: EdgeInsets.all(5),
-                          child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                TextCard(
-                                    title: "Thank you!",
-                                    text:
-                                        "Your entry has been submitted, we will be in touch shortly!"),
-                                Padding(
-                                  padding: const EdgeInsets.all(200),
-                                ),
-                              ]));
-                    });
-                  } else {
-                    // response is bad
+            Padding(
+              padding: const EdgeInsets.all(10),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(
+                  FontAwesomeIcons.building,
+                  color: Colors.white,
+                ),
+                labelText: 'Detachment',
+                hintText: 'Det 538',
+                fillColor: Colors.white,
+                filled: true,
+              ),
+              style: TextStyle(
+                color: Colors.blue[900],
+                decorationColor: Colors.white,
+              ),
+              onChanged: (String value) {
+                this.detachment = value;
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(
+                  Icons.email,
+                  color: Colors.white,
+                ),
+                labelText: 'Email',
+                hintText: 'yourname@college.edu',
+                fillColor: Colors.white,
+                filled: true,
+              ),
+              style: TextStyle(
+                color: Colors.blue[900],
+                decorationColor: Colors.white,
+              ),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              onChanged: (String value) {
+                this.email = value;
+              },
+              validator: (String value) {
+                return (value != null && value.contains("@")) &&
+                        value.contains(".")
+                    ? null
+                    : 'Invalid email address';
+              },
+            ),
+            MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: GestureDetector(
+                child: const Icon(
+                  Icons.play_arrow,
+                  size: 100,
+                  color: Colors.white,
+                ),
+                onTap: () {
+                  if (this.name == null ||
+                      this.rank == null ||
+                      this.position == null ||
+                      this.detachment == null ||
+                      this.email == null) {
+                    // for not filled out
                     setState(() {
                       this._errorOpacity = 1;
-                      this._errorMessage =
-                          "Error: connection to server failed, please check your internet connection and try again!";
+                      this._errorMessage = "Please fill out all fields!";
+                    });
+                  } else {
+                    var response = sendRequest();
+                    response.then((value) {
+                      if (value.statusCode == 200) {
+                        // response is good
+                        setState(() {
+                          this._errorOpacity = 0;
+                          this._myAnimatedWidget = Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    TextCard(
+                                        title: "Thank you!",
+                                        text:
+                                            "Your entry has been submitted, we will be in touch shortly!"),
+                                    Padding(
+                                      padding: const EdgeInsets.all(200),
+                                    ),
+                                  ]));
+                        });
+                      } else {
+                        // response is bad
+                        setState(() {
+                          this._errorOpacity = 1;
+                          this._errorMessage =
+                              "Error: connection to server failed, please check your internet connection and try again!";
+                        });
+                      }
                     });
                   }
-                });
-              }
-            },
-          ),
-        ).showCursorOnHover.moveUpOnHover,
+                },
+              ),
+            ).showCursorOnHover.moveUpOnHover,
+          ],
+        )),
+        Padding(
+          padding: EdgeInsets.all(50),
+        ),
       ],
     );
   }
@@ -250,6 +244,12 @@ class _JoinMessageState extends State<JoinMessage> {
           opacity: this._errorOpacity,
           duration: const Duration(milliseconds: 500)),
     ]);
+  }
+
+  // Used to dynamically size the grid
+  double getPaddingSize(BuildContext context) {
+    double screenSize = MediaQuery.of(context).size.width;
+    return (screenSize / 700);
   }
 
   Future<http.Response> sendRequest() async {
